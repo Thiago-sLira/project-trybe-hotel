164b47cf1958ef61e21627b85cec2b773a20a726
Thiago Lira <thiago.slira@outlook.com>
2023-08-09 14:57:09 -0300

    <chore>: Entities of each model implemented in TrybeHotelContext

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,6 +4,13 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
+    public DbSet<City> Cities { get; set; }
+
+    public DbSet<Hotel> Hotels { get; set; }
+
+    public DbSet<Room> Rooms { get; set; }
+    
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
     public TrybeHotelContext() { }
+
 }
