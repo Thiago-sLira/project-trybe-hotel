d713bb750d2431c817c0e2c6a16c43ecad5ebe55
Thiago Lira <thiago.slira@outlook.com>
2023-08-10 09:45:53 -0300

    <chore>: Implemented method in RoomRepository to get a room by id

--- src/TrybeHotel/Repository/RoomRepository.cs
+++ src/TrybeHotel/Repository/RoomRepository.cs
@@ -14,16 +14,37 @@ namespace TrybeHotel.Repository
         // 6. Desenvolva o endpoint GET /room/:hotelId
         public IEnumerable<RoomDto> GetRooms(int HotelId)
         {
-            throw new NotImplementedException(); 
+            var content = from room in _context.Rooms
+                          join hotel in _context.Hotels on room.HotelId equals hotel.HotelId
+                          join city in _context.Cities on hotel.CityId equals city.CityId
+                          where hotel.HotelId == HotelId
+                          select new RoomDto
+                          {
+                              roomId = room.RoomId,
+                              name = room.Name,
+                              capacity = room.Capacity,
+                              image = room.Image,
+                              hotel = new HotelDto
+                              {
+                                  hotelId = hotel.HotelId,
+                                  name = hotel.Name,
+                                  address = hotel.Address,
+                                  cityId = city.CityId,
+                                  cityName = city.Name
+                              }
+                          };
+            return content;
         }
 
         // 7. Desenvolva o endpoint POST /room
-        public RoomDto AddRoom(Room room) {
-            throw new NotImplementedException(); 
+        public RoomDto AddRoom(Room room)
+        {
+            throw new NotImplementedException();
         }
 
         // 8. Desenvolva o endpoint DELETE /room/:roomId
-        public void DeleteRoom(int RoomId) {
+        public void DeleteRoom(int RoomId)
+        {
             throw new NotImplementedException();
         }
     }
